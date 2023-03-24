53743390adb55abe7c771e1cba82f636fd5fbc42
marthamwangi09 <marthamwangi09@gmail.com>
2023-03-24 13:04:39 +0300

    milestone2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb+srv://marthamwangi09:Blessed2023@cluster0.afegc9n.mongodb.net/?retryWrites=true&w=majority';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
